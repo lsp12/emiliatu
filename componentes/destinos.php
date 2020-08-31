23484c36b93bc8868925300b717475fbc046fb18
lsp12 <jonathankenny852@gmail.com>
2020-08-31 00:21:04 -0500

    haciendo dinamica pagina destino

--- componentes/destinos.php
+++ componentes/destinos.php
@@ -12,26 +12,26 @@
             </div>
             <div class="row">
             <?php
-    require_once("modelo/general.php");
-    $fila=mostrarDestino();
-    
-    foreach ($fila as $fli) {
-        
-        echo '
-        <div class="col-lg-4 col-md-6">
-                    <div class="single_destination">
-                        <div class="thumb">
-                            <img src="assets/img/destination/'.$fli["imagen"].'" alt="">
-                        </div>
-                        <div class="content">
-                            <p class="d-flex align-items-center">'.$fli["nombre"].' <a href="travel_destination.php">  02 lugares turisticos</a> </p>
-                            
-                        </div>
-                    </div>
-                </div>
-        ';
-    }
-    ?>
+                require_once("modelo/general.php");
+                $fila=mostrarDestino();
+                
+                foreach ($fila as $fli) {
+                    
+                    echo '
+                    <div class="col-lg-4 col-md-6">
+                                <div class="single_destination">
+                                    <div class="thumb">
+                                        <img src="assets/img/destination/'.$fli["imagen"].'" alt="">
+                                    </div>
+                                    <div class="content">
+                                        <p class="d-flex align-items-center">'.$fli["nombre"].' <a href="travel_destination.php">  02 lugares turisticos</a> </p>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                    ';
+                }
+            ?>
             </div>
         </div>
     </div>
