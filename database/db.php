c51b3e33da74b47f7b4d52861e0a3d70347cf777
lsp12 <jonathankenny852@gmail.com>
2020-09-15 20:24:08 -0500

    sd

--- database/db.php
+++ database/db.php
@@ -1,13 +1,13 @@
 <?php
     function connectDatabase(){
-        /* $host='sql10.freemysqlhosting.net'; */
-        $host='localhost';
-        /* $user='sql10365265'; */
-        $user='root';
-        /* $pass='EnmXtq9e5b'; */
-        $pass='';
-        /* $db='sql10365265'; */
-        $db='emiliatur-sa';
+        $host='sql10.freemysqlhosting.net';
+        /* $host='localhost'; */
+        $user='sql10365856';
+        /* $user='root'; */
+        $pass='29UlP2tmKT';
+        /* $pass=''; */
+        $db='sql10365856';
+        /* $db='emiliatur-sa'; */
         $mysqli = new mysqli($host,$user,$pass,$db);
         if($mysqli->connect_errno){
             $result = "Fallo al conectar a MySQL: " . $mysqli->connect_error;
